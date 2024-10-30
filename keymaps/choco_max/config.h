0a9b80323917d9957407cd70a37d2865a627506c
Chukozor <9273715+Chukozor@users.noreply.github.com>
2024-10-30 21:52:51 +0100

    re-ajout du debounce

--- keymaps/choco_max/config.h
+++ keymaps/choco_max/config.h
@@ -73,7 +73,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPLIT_POINTING_ENABLE
 #define POINTING_DEVICE_RIGHT
 // #define CIRQUE_PINNACLE_CURVED_OVERLAY
-// #define DEBOUNCE 10
+#define DEBOUNCE 10
 #define F_SCL 100000L
 
 
